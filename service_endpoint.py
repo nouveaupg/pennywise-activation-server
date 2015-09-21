12b4f2f48f78ef0b9544aeea5678d909512ca049
John Hill <nouveau.pg@gmail.com>
2015-09-21 03:00:44 -0400

    wsgi endpoint

--- service_endpoint.py
+++ service_endpoint.py
@@ -1,25 +1,74 @@
 from flask import Flask,request
 from core import Core
+from bitcoin_ledger import BitcoinLedger
+from bitcoin_pricing import BitcoinPricing
 import json
 import re
 app = Flask(__name__)
 
-@app.route('/activate')
+API_KEY = "76E42FA28C83";
+
+@app.route("/admin")
+def admin():
+    json_request = request.get_json()
+
+    if json_request:
+        if json_request['apiKey'] != API_KEY:
+            return json.dumps({"success":False,"msg":"Unauthorized request."})
+        if json_request['action'] == "get-statistics":
+            ledger = BitcoinLedger()
+            result = ledger.getStatistics()
+            result = json_request['tag']
+            result['success'] = True
+            return json.dumps(result)
+
+
+    return json.dumps({"success":False,"msg":"Invalid request."})
+
+@app.route('/activate',methods=['GET'])
 def activate():
     uuid = request.args.get('uuid')
     email = request.args.get('email')
 
     # regex test fields before they can get to the database...
+    # uuid format: 512FCEF8-49C2-4E9B-BA34-76E42FA28C83
+
+    uuid_regex = re.compile("[A-F0-9]{8}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{4}-[A-F0-9]{12}")
+    email_regex = re.compile("\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b")
+    if uuid_regex.match(uuid) == None:
+        result = {"success":False,"msg":"Invalid argument"}
+        return json.dumps(result)
+
+    if email:
+        if email_regex.match(email) == None:
+            result = {"success":False,"msg":"Invalid argument"}
+            return json.dumps(result)
 
     dispatch = Core()
     if dispatch:
         result = dispatch.updateForUuid(uuid,email)
         if result:
             result['success'] = True
+            # convert all the types to something that won't make json toss an
+            # exception
+            if result['datePaid']:
+                result['datePaid'] = result['datePaid'].isoformat()
+            if result['dateCreated']:
+                result['dateCreated'] = result['dateCreated'].isoformat()
+            if result['dateRefunded']:
+                result['dateRefunded'] = result['dateRefunded'].isoformat()
+            if result['bitcoinBalance']:
+                result['bitcoinBalance'] = str(result['bitcoinBalance'])
+            if result['pricePaid']:
+                result['pricePaid'] = str(result['pricePaid'])
+            if result['refundPaid']:
+                result['refundPaid'] = str(result['refundPaid'])
+            if result['currentPrice']:
+                result['currentPrice'] = str(result['currentPrice'])
             return json.dumps(result)
 
-    result = {"success":False}
+    result = {"success":False,"msg":"Service down. Please try again later."}
     return json.dumps(result)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
