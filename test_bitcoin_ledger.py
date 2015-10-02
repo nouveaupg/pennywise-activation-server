51db17609171b4f2be810c3528dd47084e92d467
John Hill <nouveau.pg@gmail.com>
2015-10-02 12:53:24 -0400

    First unit test for BitcoinLedger

---
+++ test_bitcoin_ledger.py
@@ -0,0 +1,21 @@
+import unittest
+import uuid
+import bitcoin_ledger
+
+class BitcoinLedgerUnitTests(unittest.TestCase):
+    """ Unit tests for BitcoinLedger """
+
+    def test_createNewRecord(self):
+        ledger = bitcoin_ledger.BitcoinLedger()
+        self.assertIsNotNone(ledger)
+        new_uuid = str(uuid.uuid4()).upper()
+        new_ledger_id = ledger.createLedgerRecord(uuid=new_uuid)
+        self.assertTrue(new_ledger_id > 0)
+        ledger_record = ledger.getLedgerRecord(ledger_id=new_ledger_id)
+        self.assertIsNotNone(ledger_record)
+        self.assertTrue('uuid' in ledger_record)
+        self.assertTrue(ledger_record['uuid'] == new_uuid)
+        self.assertIsNone(ledger_record['pricePaid'])
+
+if __name__ == '__main__':
+    unittest.main()
