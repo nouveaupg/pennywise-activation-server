5a370676e33daa66078b1f60591bab4979001859
John Hill <nouveau.pg@gmail.com>
2015-11-10 13:29:31 -0500

    unit test for latestRecord(offset,limit)

--- test_bitcoin_ledger.py
+++ test_bitcoin_ledger.py
@@ -17,5 +17,41 @@ class BitcoinLedgerUnitTests(unittest.TestCase):
         self.assertTrue(ledger_record['uuid'] == new_uuid)
         self.assertIsNone(ledger_record['pricePaid'])
 
+    def test_fetchLatestRecords(self):
+        ledger = bitcoin_ledger.BitcoinLedger()
+        self.assertIsNotNone(ledger)
+
+        new_uuid1 = str(uuid.uuid4()).upper()
+        new_uuid2 = str(uuid.uuid4()).upper()
+        new_uuid3 = str(uuid.uuid4()).upper()
+
+        bitcoin_addr1 = '1JrTrzBJ8LnJVk9Mkt5WccGRMrN2ysRnSQ'
+        bitcoin_addr2 = '1JrTrzBJ6LnJVk9Mkt5WccGRMrN2ysRnSQ'
+        bitcoin_addr3 = '1JrTrzBJ7LnJVk9Mkt5WccGRMrN2ysRnSQ'
+
+        new_ledger_id1 = ledger.createLedgerRecord(uuid=new_uuid1,bitcoinAddr=bitcoin_addr1)
+        new_ledger_id2 = ledger.createLedgerRecord(uuid=new_uuid2,bitcoinAddr=bitcoin_addr2,emailAddr='nouveau.pg@gmail.com')
+        new_ledger_id3 = ledger.createLedgerRecord(uuid=new_uuid3,bitcoinAddr=bitcoin_addr3)
+
+        self.assertTrue(new_ledger_id1 > 0)
+        self.assertTrue(new_ledger_id2 > 0)
+        self.assertTrue(new_ledger_id3 > 0)
+
+        latestRecords = ledger.latestRecords(0,3)
+        self.assertIsNone(latestRecords[0]['emailAddress'])
+        self.assertEqual(latestRecords[0]['uuid'],new_uuid3)
+        self.assertEqual(latestRecords[0]['id'],new_ledger_id3)
+        self.assertEqual(latestRecords[0]['bitcoinAddress'],bitcoin_addr3)
+
+        self.assertEqual(latestRecords[1]['emailAddress'],'nouveau.pg@gmail.com')
+        self.assertEqual(latestRecords[1]['uuid'],new_uuid2)
+        self.assertEqual(latestRecords[1]['id'],new_ledger_id2)
+        self.assertEqual(latestRecords[1]['bitcoinAddress'],bitcoin_addr2)
+
+        self.assertIsNone(latestRecords[2]['emailAddress'])
+        self.assertEqual(latestRecords[2]['uuid'],new_uuid1)
+        self.assertEqual(latestRecords[2]['id'],new_ledger_id1)
+        self.assertEqual(latestRecords[2]['bitcoinAddress'],bitcoin_addr1)
+
 if __name__ == '__main__':
     unittest.main()
